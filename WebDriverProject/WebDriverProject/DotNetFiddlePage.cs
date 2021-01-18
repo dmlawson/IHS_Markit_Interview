a64b1eca32e13fc160e487dd8b26bb3e206a8bd0
DM Lawson <dlawso58@ford.com>
2021-01-18 18:02:13 -0500

    Created WebDriver Tests

---
+++ WebDriverProject/WebDriverProject/DotNetFiddlePage.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System.Threading;
+using SeleniumExtras.PageObjects;
+
+namespace WebDriverProject.PageObjects
+{
+
+    class DotNetFiddlePage
+    {
+        readonly string test_url = "https://dotnetfiddle.net/";
+        private IWebDriver driver;
+
+        public DotNetFiddlePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public void OpenPage()
+        {
+            driver.Navigate().GoToUrl(test_url);
+            driver.Manage().Window.Maximize();
+        }
+
+        public void ClosePage()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+        public IWebElement GetRunButton()
+        {
+            return driver.FindElement(By.Id("run-button"));
+        }
+
+        public IWebElement GetOutputBox()
+        {
+            return driver.FindElement(By.Id("output"));
+        }
+
+        public IWebElement GetPackageSearchBar()
+        {
+            string ccs_search_bar_str = "#CodeForm > div.main.docked > div.sidebar.unselectable > div:nth-child(5) > div > div > input";
+            return driver.FindElement(By.CssSelector(ccs_search_bar_str));
+        }
+
+        private IReadOnlyList<IWebElement> GetSearchResults()
+        {
+            return driver.FindElements(By.CssSelector("#menu li"));
+        }
+
+        private IReadOnlyList<IWebElement> GetVersionList(IWebElement nunit_package)
+        {
+            return nunit_package.FindElements(By.CssSelector("ul li"));
+        }
+
+        public void ClickNUnitVersion3_12()
+        {
+            Actions builder = new Actions(driver);
+            IReadOnlyList<IWebElement> package_list_items = GetSearchResults();
+            IWebElement n_unit_list_item = null;
+
+            // iterate through each item in the list, searching for "NUnit"
+            foreach (IWebElement list_item in package_list_items)
+            {
+                if (list_item.Text == "NUnit")
+                {
+                    n_unit_list_item = list_item;
+                    break;
+                }
+            }
+
+            // Move the mouse over the "NUnit" list item
+            builder.MoveToElement(n_unit_list_item).Build().Perform();
+
+            IReadOnlyList<IWebElement> versions_list = GetVersionList(n_unit_list_item);
+
+            // wait for the versions list to load
+            while (versions_list.Count <= 1)
+            {
+                Thread.Sleep(1000);
+                versions_list = GetVersionList(n_unit_list_item);
+            }
+
+            // iterate through the versions list to find the correct version
+            IWebElement version_list_item = null;
+            foreach (IWebElement list_item in versions_list)
+            {
+                if (list_item.Text == "3.12.0")
+                {
+                    version_list_item = list_item;
+                    break;
+                }
+            }
+
+            // Move mouse to version "3.12.0" and click it
+            builder.MoveToElement(version_list_item).Click().Build().Perform();
+        }
+
+        public string GetSelectedPackageString()
+        {
+            return driver.FindElement(By.ClassName("package-name")).Text;
+        }
+    }
+}
---
+++ WebDriverProject/WebDriverProject/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using WebDriverProject.PageObjects;
+
+namespace WebDriverProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static DotNetFiddlePage page;
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            page = new DotNetFiddlePage(new ChromeDriver());
+            page.OpenPage();
+        }
+        
+        [TestMethod]
+        public void TestChromeDriver_1()
+        {
+            // Constants
+            string expected = "Hello World";
+
+            // Find and click the Run button
+            page.GetRunButton().Click();
+
+            // Find and read the output text
+            string output = page.GetOutputBox().GetAttribute("textContent").Trim();
+            // Normalize the string for comparison
+            output = Regex.Replace(output, @"\u00A0", " ");
+            // Verify that the two strings are equal
+            Assert.AreEqual(expected, output, false);
+            Console.WriteLine("Verified output matches expected value!");
+        }
+        
+        [TestMethod]
+        public void TestChromeDriver_2()
+        {
+            // Fine the NuGet Packages search bar and type "nUnit"
+            page.GetPackageSearchBar().SendKeys("nUnit");
+
+            //Move the mouse over the list item labeled "NUnit" and then click on version "3.12.0"
+            page.ClickNUnitVersion3_12();
+
+            Assert.AreEqual(page.GetSelectedPackageString(), "NUnit");
+            Console.WriteLine("Successfully Verify NUnit Package was added!");
+        }
+
+        [AssemblyCleanup]
+        public static void CleanUp()
+        {
+            page.ClosePage();
+        }
+    }
+}
